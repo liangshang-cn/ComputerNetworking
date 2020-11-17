c4ec72e3d3b9d6caf6825c49b3d2d9aceada4b38
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-11-17 21:27:07 +0800

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -4,8 +4,9 @@ host = ''
 port = 6666
 buffer_size = 1024
 address = (host,port)
-
+# 创建服务端的套接字
 serverSocket = socket(AF_INET, SOCK_DGRAM)
+# 通过绑定，显式地为套接字分配一个端口号
 serverSocket.bind(address)
 print('The server is ready to receive...')
 while True:
