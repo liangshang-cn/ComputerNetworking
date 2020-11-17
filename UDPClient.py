754ae027e61b2204a4ca6a32ffc8bb0e0dfde3e3
liangshang-cn <66558919+liangshang-cn@users.noreply.github.com>
2020-11-17 21:26:30 +0800

    Update UDPClient.py

--- UDPClient.py
+++ UDPClient.py
@@ -5,9 +5,11 @@ port = 6666
 buffer_size = 1024
 address = (host,port)
 
+# 创建客户端的套接字，参数AF_INET表示IPv4，SOCK_DGRAM表示是个UDP套接字
 clientSocket = socket(AF_INET,SOCK_DGRAM)
 message = input("Input lowercase sentence:")
 clientSocket.sendto(message.encode(), (host, port))
+# 输入时字节流通过encode编码为字符串，输出时字符串通过decode解码为字节
 modifiedMessage, addr = clientSocket.recvfrom(buffer_size)
 print(modifiedMessage.decode())
 clientSocket.close()
